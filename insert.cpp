cdbceb4364c69d367217b743ae052caf2b5cc4ca
Januuu26 <b77286116@gmail.com>
2026-03-12 19:14:09 +0700

    menambahkan else

--- insert.cpp
+++ insert.cpp
@@ -12,4 +12,62 @@ void input(){
         if (n <= 20){
         break;
     }
-    
+        else {
+        cout << "\nArray tidak boleh lebih dari 20\n" ;
+        
+    }
+
+}
+
+    cout << endl;
+    cout << "======================================";
+    cout << "=========Masukan elemen array=========";
+    cout << "======================================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
+}
+
+void insertionsort() {
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++) 
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) 
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep " << i << ":";
+        for (int k = 0; k < n; k++) 
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
+
+void display()
+{
+    cout << "\n====================";
+    cout << "\nEelement Array" << endl;
+    cout << "====================" << endl;
+
+    for (int j = 0; j < n; j++) 
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
