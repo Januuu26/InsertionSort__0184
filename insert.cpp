5c59e73df8700ac77fdd82375f5e48251b8bc84a
Januuu26 <b77286116@gmail.com>
2026-03-12 19:12:14 +0700

    membuat input

--- insert.cpp
+++ insert.cpp
@@ -1,3 +1,15 @@
 #include <iostream> 
 using namespace std;
 
+int arr[20]; 
+int n;
+
+void input(){
+    while (true) {
+        cout << "Masukan jumlah data pada array:";
+        cin >> n;
+
+        if (n <= 20){
+        break;
+    }
+    
