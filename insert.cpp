d99bd3baedd221f9d39cf13c7f593d7765feafdd
Januuu26 <b77286116@gmail.com>
2026-03-12 19:26:48 +0700

    menambahkan void untuk menampiilkan data

--- insert.cpp
+++ insert.cpp
@@ -51,4 +51,18 @@ void insertionsort() {
         }
     }
 }
+void display()
+{
+    cout << "\n====================";
+    cout << "\nEelement Array" << endl;
+    cout << "====================" << endl;
+
+    for (int j = 0; j < n; j++) 
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+
 
